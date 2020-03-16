feb8ed53bf901079fc3e1151c87a6cbf3a67cd59
freya-song <freyaa1103@gmail.com>
2020-03-15 22:08:06 -0400

    Update sentiment_model.py

--- Local_Training/model_training/sentiment_model.py
+++ Local_Training/model_training/sentiment_model.py
@@ -21,43 +21,42 @@ def keras_model_cnn(embedding_matrix, config):
     Creating a CNN model for sentiment modeling
 
     """
-    # cnn_model = models.Sequential()
-    # cnn_model.add(
-    #     layers.Embedding(
-    #         input_length = config['padding_size'],
-    #         input_dim = config['embeddings_dictionary_size'],
-    #         output_dim = config['embeddings_vector_size'],
-    #         weights = [embedding_matrix],
-    #         trainable = True,
-    #         name = 'embedding')
-    # )
-    # cnn_model.add(
-    #     layers.Conv1D(
-    #         filters = 100,
-    #         kernel_size = 2,
-    #         strides = 1,
-    #         padding = 'valid',
-    #         activation='relu')
-    # )
-    # cnn_model.add(
-    #     layers.GlobalMaxPool1D()
-    # )
-    # cnn_model.add(
-    #     layers.Dense(
-    #         units = 100,
-    #         activation='relu')
-    # )
-    # cnn_model.add(
-    #     layers.Dense(
-    #         units = 1,
-    #         activation='sigmoid')
-    # )
-    # cnn_model.compile(
-    #     optimizer='adam',
-    #     loss=tf.keras.losses.binary_crossentropy,
-    #     metrics=['accuracy'])
-    # return cnn_model
-    return tf.keras.models.load_model('models/sentiment_model_CNN.h5/1')
+    cnn_model = models.Sequential()
+    cnn_model.add(
+        layers.Embedding(
+            input_length = config['padding_size'],
+            input_dim = config['embeddings_dictionary_size'],
+            output_dim = config['embeddings_vector_size'],
+            weights = [embedding_matrix],
+            trainable = True,
+            name = 'embedding')
+    )
+    cnn_model.add(
+        layers.Conv1D(
+            filters = 100,
+            kernel_size = 2,
+            strides = 1,
+            padding = 'valid',
+            activation='relu')
+    )
+    cnn_model.add(
+        layers.GlobalMaxPool1D()
+    )
+    cnn_model.add(
+        layers.Dense(
+            units = 100,
+            activation='relu')
+    )
+    cnn_model.add(
+        layers.Dense(
+            units = 1,
+            activation='sigmoid')
+    )
+    cnn_model.compile(
+        optimizer='adam',
+        loss=tf.keras.losses.binary_crossentropy,
+        metrics=['accuracy'])
+    return cnn_model
 
 def keras_model_rnn(embedding_matrix, config):
     """
